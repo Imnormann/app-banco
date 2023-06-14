abf759666447921aa976aa01bedc5563d4b7d37a
Imnormann <norbertosalvadormartinez@gmail.com>
2023-06-14 12:11:09 +0200

    inplementado usuario

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClienteService } from 'src/app/services/cliente/cliente.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = "";
+  password: string = "";
 
+  constructor(private clienteService: ClienteService, private router: Router){}
+
+  validarLogin() {
+    console.log('Validando...', this.email, this.password);
+const usuario = this.clienteService.hacerlogin(this.email, this.password);
+if (usuario){
+  console.log(usuario);
+  this.router.navigateByUrl("/pages/dashboard");
+}else{
+  console.error('credenciales incorrectas');
+}
+  }
 }
---
+++ src/app/datos/usuarios-ejemplo.ts
@@ -0,0 +1,18 @@
+export const clientes = [
+    {
+        id:1,
+        idGestor: 12,
+        correo: "ejemplo@correo,com",
+        password:"unapass",
+        usuario:"Paco",
+        saldo: 10.58,
+    },
+    {
+        id: 2,
+        idGestor: 12,
+        correo: "ana@correo,com",
+        password:"unapass",
+        usuario:"Ana",
+        saldo: 10.58,
+    }
+]

ad5c6c2045bf94ddd5f9f85ddc0120c44fdb127a
Imnormann <norbertosalvadormartinez@gmail.com>
2023-06-14 14:17:45 +0200

    añadimos modulo transferencias sin terminar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { TablaTransferenciasComponent } from './components/tabla-transferencias/tabla-transferencias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaTransferenciasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
   ],
+  exports: [TablaTransferenciasComponent],
   providers: [],
   bootstrap: [AppComponent]
 })

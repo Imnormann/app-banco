b67c985e3be4e93fb7cbc87418b3bac19d01773f
Imnormann <norbertosalvadormartinez@gmail.com>
2023-06-14 14:10:33 +0200

    añadida paginas de transferencias

---
+++ src/app/datos/transferencias ejemplo.ts
@@ -0,0 +1,53 @@
+export const transferenciasRecibidas = [
+    {
+        id: 1,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 50,
+        concepto: "regalo cumpleaños",
+        fecha: new Date(),
+    },
+    {
+        id: 2,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 50,
+        concepto: "regalo cumpleaños",
+        fecha: new Date(),
+    },
+    {
+        id: 3,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 50,
+        concepto: "regalo cumpleaños",
+        fecha: new Date(),
+    },
+  
+];
+export const transferenciasEnviadas = [
+    {
+        id: 1,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 50,
+        concepto: "regalo cumpleaños",
+        fecha: new Date(),
+    },
+    {
+        id: 2,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 50,
+        concepto: "regalo cumpleaños",
+        fecha: new Date(),
+    },
+    {
+        id: 3,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 50,
+        concepto: "regalo cumpleaños",
+        fecha: new Date(),
+    },
+];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransferenciasRecibidasComponent } from './transferencias-recibidas/transferencias-recibidas.component';
+import { TransferenciasEnviadasComponent } from './transferencias-enviadas/transferencias-enviadas.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, TransferenciasRecibidasComponent, TransferenciasEnviadasComponent],
   imports: [
     CommonModule,
     PagesRoutingModule
